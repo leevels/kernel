c34bf46a16b151c08a44722ec729259549848779
XiangMin Liu <635750556@qq.com>
2019-05-08 22:23:49 +0800

    refactor(facade): rename facade to proxy

--- Proxy/App.php
+++ Proxy/App.php
@@ -18,12 +18,12 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Leevel\Kernel\Facade;
+namespace Leevel\Kernel\Proxy;
 
 use Leevel\Di\Container;
 
 /**
- * 门面 app.
+ * 代理 app.
  *
  * @author Xiangmin Liu <635750556@qq.com>
  *
